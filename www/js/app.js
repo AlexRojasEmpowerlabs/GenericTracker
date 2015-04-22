bbda17a6e44b923d7791bbaac6be6912d2a9d88b
AlexRojasEmpowerlabs <alexrojas@Juan-Heredias-MacBook-Pro.local>
2015-04-22 16:08:24 -0500

    alta completada

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,51 @@
 (function(){
   'use strict';
   var todosTicket={};
-  var module = angular.module('app', ['onsen']);
+  var module = angular.module('app', ['onsen'],function($httpProvider) {
+  // Use x-www-form-urlencoded Content-Type
+  $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+
+  /**
+   * The workhorse; converts an object to x-www-form-urlencoded serialization.
+   * @param {Object} obj
+   * @return {String}
+   */ 
+  var param = function(obj) {
+    var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
+
+    for(name in obj) {
+      value = obj[name];
+
+      if(value instanceof Array) {
+        for(i=0; i<value.length; ++i) {
+          subValue = value[i];
+          fullSubName = name + '[' + i + ']';
+          innerObj = {};
+          innerObj[fullSubName] = subValue;
+          query += param(innerObj) + '&';
+        }
+      }
+      else if(value instanceof Object) {
+        for(subName in value) {
+          subValue = value[subName];
+          fullSubName = name + '[' + subName + ']';
+          innerObj = {};
+          innerObj[fullSubName] = subValue;
+          query += param(innerObj) + '&';
+        }
+      }
+      else if(value !== undefined && value !== null)
+        query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+    }
+
+    return query.length ? query.substr(0, query.length - 1) : query;
+  };
+
+  // Override $http service's default transformRequest
+  $httpProvider.defaults.transformRequest = [function(data) {
+    return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+  }];
+});
 
   module.controller('AppController', function($scope, $data) {
     $scope.doSomething = function() {
@@ -47,5 +91,21 @@
       
       return data;
   });
+  module.controller('NuevoController',function($scope,$http){
+  	$scope.formData = {};
+  	$scope.push=function(){
+  		modal.show();
+  		$http.post('http://empowerlabs.com/proyectos/soporte/nuevo.php', $scope.formData).
+  success(function(data, status, headers, config) {
+  	modal.hide();
+  	$scope.alta.$setPristine();
+  	$scope.formData = {};
+ons.notification.alert({message: ''+data.mensaje, title:"Intellibanks"});
+  }).
+  error(function(data, status, headers, config) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+  	};
+  });
 })();
-
